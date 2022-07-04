8bfebf10fe89f7c3f4847128274d58ef8d019379
Your Name <jouxjr7>
2022-07-04 08:45:03 -0500

    componente menu agregado

--- react1/estado/src/App.js
+++ react1/estado/src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import LightSwitch from './Components/LightSwitch';
+import Menu from './Components/Menu';
+
 
 function App() {
   return (
     <div className="App">
       <LightSwitch/>
+      <Menu/>
       
     </div>
   );
---
+++ react1/estado/src/Components/Menu.jsx
@@ -0,0 +1,14 @@
+import  React  from 'react';
+class Menu extends React.Component{
+    render(){
+        return(
+        <div>
+            <ul>
+                <li> Home</li>
+                <li>Products</li>
+                <li>Contact us</li>
+            </ul>
+        </div>)
+    }
+}
+export default Menu;
