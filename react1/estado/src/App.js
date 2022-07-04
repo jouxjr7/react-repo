5f2a52e7ab65006b24ff07a0e597f14e4dddc66a
Your Name <jouxjr7>
2022-07-04 13:29:05 -0500

    correcto

--- react1/estado/src/App.js
+++ react1/estado/src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import LightSwitch from './Components/LigthSwitch';
+import LightSwitch from './Components/LightSwitch';
 
 function App() {
   return (
